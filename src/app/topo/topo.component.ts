90145fd091b735039e33180f825907963b9de7f0
Breno Robert <64442888+BrenoRobertDiogo@users.noreply.github.com>
2021-07-02 10:04:07 -0300

    alguns seletores e atribuição de css

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,6 +3,9 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-topo',
     templateUrl: './topo.component.html',
+    // template: '<h1>teste pra ver se ta funfando</h1>' // sobrescreve i templateUrl
+    styles: ['.exemplo { color: black }'], // adicionando style
+    styleUrls: ['./topo.component.css']
      
 })
 export class TopoComponent {
